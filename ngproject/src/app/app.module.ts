6bd5b26b83aa7332c745d96c057cd4fcf4a8e0c5
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-07-10 18:51:11 +0600

    1st project

---
+++ ngproject/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LocationComponent,
+    CreatelocationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
